fbcdea1530963ce5f5f2b29c86bb7934c124b520
Harshil <harshilgoti01@gmail.com>
2025-04-24 01:38:59 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'whistlewatch',
+  webDir: 'dist'
+};
+
+export default config;
